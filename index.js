e22f5f468128c4816a45f2855cc746a43bbf8bc3
Susana Solis Flores <Suzy.flores@icloud.com>
2023-10-07 13:36:39 -0400

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(name, email) {
+    const submitData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(submitData)
+    };
+  
+    return fetch('http://localhost:3000/users', configurationObject)
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Unauthorized Access');
+        }
+      })
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML += id;
+        return id;
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        document.body.innerHTML += errorMessage;
+        console.log(errorMessage);
+      });
+  }
